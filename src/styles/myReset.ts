03d6ff04aa3fdb3e7f00d9442e46420487f31c43
d5ng <dong5ffice@gmail.com>
2024-03-14 14:28:20 +0900

    Style: 간단한 CSS 수정

--- src/styles/myReset.ts
+++ src/styles/myReset.ts
@@ -19,6 +19,8 @@ const myReset = css`
     font-weight: 400;
     min-height: 100vh;
     position: relative;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   ol,
