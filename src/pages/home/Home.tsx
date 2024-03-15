3fbf6a956c6ca1ab38936340b7fb1ce963d0cd02
78-artilleryman <ywy040150@gmail.com>
2024-03-15 14:50:07 +0900

    rename: main 폴더 home으로 변경

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import FilterList from '../home/components/filterList/FilterList';
+import SearchBar from '../home/components/searchBar/SearchBar';
+import PostList from '../home/components/postLIst/PostList';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <section>
+      <FilterList />
+      <SearchBar />
+      <PostList />
+    </section>
+  );
 }
 
 export default Home;
