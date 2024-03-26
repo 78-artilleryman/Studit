55955e9923c480708554f32bbc58a8d5096445d2
78-artilleryman <ywy040150@gmail.com>
2024-03-27 00:49:00 +0900

    feat: TechnologyFilter context 연결

--- src/pages/home/components/technologyFilter/TechnologyItem.tsx
+++ src/pages/home/components/technologyFilter/TechnologyItem.tsx
@@ -1,3 +1,4 @@
+import { useFilter } from '@Pages/home/context/FilterContext';
 import * as S from './TechnologyItem.style';
 
 interface TechnologyItemProps {
@@ -7,8 +8,11 @@ interface TechnologyItemProps {
 }
 
 function TechnologyItem({ name, image, color }: TechnologyItemProps) {
+  const { filterState, onChagTechnologysFilter } = useFilter();
+  const checked = filterState.technologys.includes(name);
+
   return (
-    <S.TechnologyItem color={color}>
+    <S.TechnologyItem color={color} $checked={checked} onClick={() => onChagTechnologysFilter(name)}>
       <S.Image src={image} alt="TechnologyItem" />
       <S.Name>{name}</S.Name>
     </S.TechnologyItem>
