ed90ff1e60485ab4dbe85f066b568571492e7414
78-artilleryman <ywy040150@gmail.com>
2024-03-28 19:22:11 +0900

    feat: 데이터베이스에 게시물 가져오는 기능 구현

--- src/pages/home/components/postLIst/PostList.tsx
+++ src/pages/home/components/postLIst/PostList.tsx
@@ -1,6 +1,11 @@
 import PostItem from '../postItem/PostItem';
 import styled from 'styled-components';
 import { data } from './mockdata';
+import { useEffect, useState } from 'react';
+import { db } from '@config/firebaseApp';
+import { useFilter } from '@Pages/home/context/FilterContext';
+import { buildFirestoreQuery } from '@Pages/home/service/Filter';
+import { onSnapshot } from 'firebase/firestore';
 
 const PostLayout = styled.div`
   display: grid;
@@ -10,6 +15,26 @@ const PostLayout = styled.div`
   margin: 0 auto;
 `;
 function PostList() {
+  const [postData, setPostData] = useState({});
+  const { filterState } = useFilter();
+  const { studyType, period, technologys } = filterState;
+
+  console.log(postData);
+
+  useEffect(() => {
+    const postsQuery = buildFirestoreQuery(db, studyType, period, technologys);
+
+    const unsubscribe = onSnapshot(postsQuery, snapshot => {
+      const data = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setPostData(data);
+    });
+
+    return () => unsubscribe();
+  }, [studyType, period, technologys]);
+
   return (
     <PostLayout>
       {data.map(post => (
