79eac674b87bea85feef0d5cd4cc69fa738f5b3b
78-artilleryman <ywy040150@gmail.com>
2024-04-09 23:12:36 +0900

    feat: 필터 바깥 영역 클릭시 필터가 닫히게 코드 추가

--- src/hooks/useSelect.ts
+++ src/hooks/useSelect.ts
@@ -1,8 +1,9 @@
-import { useState, MouseEvent } from 'react';
+import { useState, MouseEvent, useRef, useEffect, useCallback } from 'react';
 
 function useSelect() {
   const [isSelectOpen, setIsSelectOpen] = useState(false);
   const [selected, setSelected] = useState('');
+  const ref = useRef<HTMLDivElement>(null);
 
   const selectToggleHandler = () => setIsSelectOpen(prevSelect => !prevSelect);
 
@@ -14,7 +15,25 @@ function useSelect() {
     setSelected(target.textContent!);
   };
 
+  const handleClickOutside = useCallback(
+    (evnet: MouseEvent) => {
+      if (!ref.current) return;
+
+      const inside = ref.current.contains(evnet.target as Node);
+      if (!inside) setIsSelectOpen(false);
+    },
+    [setIsSelectOpen, ref],
+  );
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside as any);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside as any);
+    };
+  }, [handleClickOutside]);
+
   return {
+    ref,
     isSelectOpen,
     selected,
     selectToggleHandler,
